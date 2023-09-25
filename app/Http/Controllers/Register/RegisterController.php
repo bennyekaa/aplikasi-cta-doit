2bbf4eddac626f3c8cf097c7d739df0701531d31
bennyekaa <benny.eka@um.ac.id>
2023-09-25 08:30:23 +0700

    pertama

---
+++ app/Http/Controllers/Register/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Register;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index(){
+        return view('register.index');
+    }
+}
